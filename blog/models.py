434709facc30aa3a9ed254c4fac971a051d90a76
Bonaventure-nwaokoro <tochinail5@gmail.com>
2019-03-06 01:18:14 +0100

    testing 2

--- blog/models.py
+++ blog/models.py
@@ -21,11 +21,24 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
 
-# class Comment(models.Model):
-#     name = models.CharField(max_length=100)
-#     body = models.TextField()
-#     pub_date = models.DateTimeField(default=timezone.now)
-#     article = models.ForeignKey(Post)
-    
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+
 
 
